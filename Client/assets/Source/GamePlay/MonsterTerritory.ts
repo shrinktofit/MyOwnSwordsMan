9472ff288ab4097ebc9ba5d6f11beb308ff3a9a9
Leslie Leigh <leslieleigh@outlook.com>
2021-06-30 00:02:09 +0800

    Update

--- Client/assets/Source/GamePlay/MonsterTerritory.ts
+++ Client/assets/Source/GamePlay/MonsterTerritory.ts
@@ -10,6 +10,12 @@ export class MonsterTerritory extends cc.Component {
     @cc._decorator.property
     public capacity = 0;
 
+    @cc._decorator.property
+    public minScale = 1.0;
+
+    @cc._decorator.property
+    public maxScale = 1.0;
+
     @cc._decorator.property(cc.Prefab)
     public prefab!: cc.Prefab;
 
@@ -22,11 +28,23 @@ export class MonsterTerritory extends cc.Component {
             const item = cc.instantiate(this.prefab);
             this.node.addChild(item);
 
+            // Random position
             const angle = cc.randomRange(0.0, Math.PI * 2);
             const radius = cc.randomRange(0.0, this.radius);
             const pX = radius * Math.cos(angle);
             const pY = radius * Math.sin(angle);
-            item.setPosition(pX, item.position.y, pY);
+            const position = cc.Vec3.add(new cc.Vec3(), item.position, new cc.Vec3(pX, 0.0, pY));
+            item.setPosition(position);
+
+            // Random rotation
+            const faceAngle = cc.randomRange(0.0, Math.PI * 2);
+            const rotation = cc.Quat.rotateY(new cc.Quat(), item.rotation, faceAngle);
+            item.setRotation(rotation);
+
+            const scaleX = cc.randomRange(this.minScale, this.maxScale);
+            const scaleY = cc.randomRange(this.minScale, this.maxScale);
+            const scaleZ = cc.randomRange(this.minScale, this.maxScale);
+            item.setScale(new cc.Vec3(scaleX, scaleY, scaleZ));
         }
     }
 
